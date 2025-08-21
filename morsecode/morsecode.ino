e1059a267cfea1f1f15f6539488f02a98d12d2b8
Caitex212 <firewizard787@gmail.com>
2025-08-21 09:20:55 +0200

    working beeper and fixed a few bugs

--- morsecode/morsecode.ino
+++ morsecode/morsecode.ino
@@ -1,58 +1,67 @@
-int ledPin = 13;
+int pin = 10;
 
 char* letters[] = {
-".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", // A-I
-".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // J-R 
-"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." // S-Z
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", // A-I
+  ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // J-R 
+  "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." // S-Z
 };
 
 char* numbers[] = {
   "-----", ".----", "..---", "...--", "....-", ".....",
-"-....", "--...", "---..", "----."
+  "-....", "--...", "---..", "----."
 };
 
-int timeUnit = 100;
+int timeUnit = 50;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(pin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char receivedChar = Serial.read();
+    
     if (isspace(receivedChar)) {
       send_morse_space();
-    } else if (isalpha(receivedChar)) {
+    } 
+    else if (isalpha(receivedChar)) {
       char lowerChar = tolower(receivedChar);
       send_morse(letters[lowerChar - 'a']);
-    } else if (isdigit(receivedChar)) {
+    } 
+    else if (isdigit(receivedChar)) {
       int digitIndex = receivedChar - '0';
       send_morse(numbers[digitIndex]);
     }
   }
-
 }
 
 void send_morse(String character) {
   for (int i = 0; i < character.length(); i++) {
     if (character[i] == '-') {
-      digitalWrite(ledPin, HIGH);
-      delay(3 * timeUnit);                
-      digitalWrite(ledPin, LOW);
+      tone(pin, 3000, 3 * timeUnit);
+      delay(3 * timeUnit);
+      //analogWrite(ledPin, 100);
+      //delay(3 * timeUnit);
+      //analogWrite(ledPin, 0);
+      //lay(timeUnit);
+      Serial.println("_");
+    } 
+    else if (character[i] == '.') {
+      tone(pin, 3000, timeUnit);
       delay(timeUnit);
-    } else if (character[i] == '.') {
-      digitalWrite(ledPin, HIGH);
-      delay(timeUnit);   
-      digitalWrite(ledPin, LOW);
-      delay(timeUnit); 
+      //analogWrite(ledPin, 100);
+      //delay(timeUnit);
+      //analogWrite(ledPin, 0);
+      //delay(timeUnit);
+      Serial.println(".");
     }
-    delay(3 * timeUnit);
+    delay(timeUnit);
   }
+  delay(2 * timeUnit);
   Serial.println("char done: " + character);
 }
 
 void send_morse_space() {
-  delay(4 * timeUnit); // 4+3 are 7 (We already have a delay of 3 in send_morse)
+  delay(7 * timeUnit);
 }
-
